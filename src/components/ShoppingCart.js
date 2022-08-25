ea84ee91e10890e50562d18f4515d9f81ed5df03
Sophlanz <sophialanzendorf@gmail.com>
2022-08-25 14:17:38 +0200

    calculate all items in shopping bag, and display final rpice

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -2,16 +2,31 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import CartItem from './CartItem';
 import uniqid from 'uniqid';
+import { useState, useEffect } from 'react';
 const ShoppingCart = () => {
+    const [total,setTotal] = useState(0);
     const items = useSelector((state) => state.bag)
+    const calcTotal = (items) => {
+        //reset to 0
+        setTotal(0);
+        items.map((item) => (
+            setTotal((prev)=> prev + Number(item.price.substr(1)))
+        ))
+    }
+    useEffect(() => {
+        calcTotal(items);
+  }, []);
     return(  
  <ul className = "shoppingCart">
    
        { items.map((item)=> (
            <li key={uniqid()} >
         <CartItem title ={item.title} price = {item.price} date = {item.date} id = {item.id}/> 
+        
         </li>
        ))}
+     
+       <p>${total}</p>
 
 </ul>
         )
