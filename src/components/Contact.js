849123f70c77f3e447775067ee658a5ac2a81380
Sophlanz <sophialanzendorf@gmail.com>
2022-09-05 20:08:16 +0200

    video added to about page, styling to buttons, and styling to contact information

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Navbar from './Navbar';
+import videoSurf from '../videos/video2.mp4';
 const Contact = () => {
     return(
-        <div>
+            <div>
             <Navbar/>
-            <div className="contactHeader"/>
+            <div className="contactHeader">
+                 <video className="surf"controls autoPlay loop muted>
+                     <source src={videoSurf} type="video/mp4"></source>
+                 </video>
+            </div>
+            <div className="aboutContent">
             <div className="signUpContainer">
                 <h1><span className="firstPart">DON'T BE SHY,</span><span className="lastPart">SAY HI.</span></h1>
                 <form>
@@ -20,18 +26,36 @@ const Contact = () => {
                     </div>
                         <div  className="inputField">
                                 <label>Number</label>
-                                <input type="number" placeholder="Your Number"/>
+                                <input className="number" type="number" placeholder="Your Number"/>
                         </div>
                         <div  className="inputField">
                                 <label>Your Message</label>
-                                <input type="text" placeholder="Tell us how we can help you"/>
+                                <textarea className="message" type="textarea" rows={5}cols={5} placeholder="Tell us how we can help you"/>
                         </div>
+                        <button type="submit">Send</button>
                 </form>
-                <div className="tripQueries">
-
+            </div>
+            <div className="excursionQueries">
+                    <div className="queriesTop">
+                        <h1>Excursion Queries</h1>
+                        <h2>Require information about an excursion?</h2>
+                        <p className="queryContact">excursions@excursionescape.com</p>
+                        <h2>Opening Hours</h2>
+                        <p className="hours">Mon-Fri: 9am-5pm</p>
+                        <h2>*We will do our best to get back to you as soon as possible,
+                        but it may take up to 48 hours.</h2> 
+                    </div>
+                    <div className="queriesBottom">
+                        <h2>Emergency Helpline</h2>
+                        <p className="queryContact">555 2222 4444</p>
+                        <h2>Contactable Hours</h2>
+                        <p className="hours">Mon-Sun: 24 Hours</p>
+                    </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        
+        
 
     )
 }
