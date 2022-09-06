4bbd3012e17efd5b1e346306291e4d850c641566
Sophlanz <sophialanzendorf@gmail.com>
2022-09-06 14:58:40 +0200

    fixed spelling error surfing

--- src/components/excursions/Surfing.js
+++ src/components/excursions/Surfing.js
@@ -4,7 +4,7 @@ import image from '../../images/kuta.jpg';
 const KutaBeach = () => {
     return (
         <div>
-            <Layout country = "Indonesia" title = "Surfind On Kuta Beach"
+            <Layout country = "Indonesia" title = "Surfing On Kuta Beach"
  introduction="Quisque eu ultricies ex, ac rutrum ligula. Mauris molestie vehicula nisi, nec rutrum lacus auctor et. "
 overview="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id lectus suscipit eros dignissim imperdiet at sed erat. Fusce in fringilla mauris. Proin tincidunt lacinia pulvinar. Sed massa enim, cursus vitae cursus a, varius ac velit.Nam lectus dui, pellentesque quis nibh ut, condimentum sodales ante.  et netus et malesuada fames ac turpis egestas. Etiam velit lorem, blandit a urna id, eleifend pulvinar urna. "
 transportType ="Bus"
