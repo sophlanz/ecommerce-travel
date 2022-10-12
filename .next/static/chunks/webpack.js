47682dd54d6e064bda9959d0650c78b5ddfc9548
Sophlanz <sophialanzendorf@gmail.com>
2022-10-11 20:24:17 -0400

    marquee componenet converted to nextjs image optimzation and added to _app.js to be displayed on all pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b62102c114a0f620"; }
+/******/ 		__webpack_require__.h = function() { return "c2b18e63495fb14a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -1,55 +1,101 @@
 import React from "react";
-import marq1 from "../../public/images/marq1.jpg";
-import marq2 from "../../public/images/marq2.jpg";
-import marq3 from "../../public/images/marq3.jpg";
-import marq4 from "../../public/images/marq4.jpg";
-import marq5 from "../../public/images/marq5.jpg";
-import marq6 from "../../public/images/marq6.jpg";
-import marq7 from '../../public/images/marq7.jpg';
-import marq8 from '../../public/images/marq8.jpg';
-import marq9 from '../../public/images/marq9.jpg';
-import marq10 from '../../public/images/marq10.jpg';
-import marq11 from '../../public/images/marq11.jpg';
-import marq12 from '../../public/images/marq12.jpg';
-import boat from '../../public/images/boat.jpg';
-import beach from '../../public/images/beach.jpg';
-import komodo from '../../public/images/komodo.jpg';
+import Image from 'next/image';
 const Marquee = () => {
     return(
-       <div class="marquee">
-       <div class="marquee-content" >
-           <img src={boat} alt="Natural"  class="boat"/>
-           <img src={marq1} alt="Natural"  class="marq1"/>
-           <img src={marq4} alt="Natural"  class="marq4"/>
-           <img src={beach}  alt="Natural" class="marqBeach"/>
-           <img src={marq3} alt="Natural"  class="marq3"/>
-           <img src={marq8} alt="Natural"  class="marq8"/>
-           <img src={marq10} alt="Natural"  class="marq10"/>
-           <img src={marq12} alt="Natural"  class="marq12"/>
-           <img src={komodo}  alt="Natural" />
-           <img src={marq2} alt="Natural"  class="marq2"/>
-           <img src={marq5} alt="Natural"  class="marq5"/>
-           <img src={marq6} alt="Natural"  class="marq6"/>
-           <img src={marq7} alt="Natural"  class="marq7"/>
-           <img src={marq9} alt="Natural"  class="marq9"/>
-           <img src={marq11} alt="Natural"  class="marq11"/>
+       <div className="marquee">
+       <div className="marquee-content" >
+       <div>
+                <Image src='/images/boat.jpg' alt="Natural"   height="160px" width="160px"className="boat"/>
+            </div>
+            <div>
+                <Image src='/images/marq1.jpg' alt="Natural"  height="160px" width="160px"  className="marq1"/>
+            </div>
+           <div>
+                <Image src='/images/marq4.jpg' alt="Natural"  height="160px" width="160px"  className="marq4"/>
+           </div>
+           <div>
+                <Image src='/images/beach.jpg'  alt="Natural" height="160px" width="160px"  className="marqBeach"/>
+           </div>
+           <div>
+                <Image src='/images/marq3.jpg' alt="Natural"  height="160px" width="160px"  className="marq3"/>
+           </div>
+           <div>
+                <Image src='/images/marq8.jpg' alt="Natural"  height="160px" width="160px"  className="marq8"/>
+           </div>
+           <div>
+                <Image src='/images/marq10.jpg' alt="Natural" height="160px" width="160px"  className="marq10"/>
+           </div>
+           <div>
+                <Image src='/images/marq12.jpg' alt="Natural" height="160px" width="160px"  className="marq12"/>
+           </div>
+           <div>
+                <Image src='/images/komodo.jpg'  alt="Natural"  height="160px" width="160px"  className="komodo"/>
+           </div>
+           <div>
+                <Image src='/images/marq2.jpg' alt="Natural"    height="160px" width="160px"  className="marq2"/>
+           </div>
+           <div>
+                <Image src= '/images/marq5.jpg' alt="Natural"   height="160px" width="160px"   className="marq5"/>
+           </div>
+           <div>
+                <Image src= '/images/marq6.jpg' alt="Natural"   height="160px" width="160px"   className="marq6"/>
+           </div>
+           <div>
+                <Image src= '/images/marq7.jpg' alt="Natural"   height="160px" width="160px"   className="marq7"/>
+           </div>
+           <div>
+                <Image src= '/images/marq9.jpg' alt="Natural"   height="160px" width="160px"   className="marq9"/>
+           </div>
+           <div>
+                <Image src= '/images/marq11.jpg' alt="Natural" height="160px" width="160px"   classNames="marq11"/>
+           </div>
        </div>
-       <div class= "marquee-content" aria-hidden="true" >
-       <img src={boat} alt="Natural"  class="boat"/>
-           <img src={marq1} alt="Natural"  class="marq1"/>
-           <img src={marq4} alt="Natural"  class="marq4"/>
-           <img src={beach}  alt="Natural" class="marqBeach"/>
-           <img src={marq3} alt="Natural"  class="marq3"/>
-           <img src={marq8} alt="Natural"  class="marq8"/>
-           <img src={marq10} alt="Natural"  class="marq10"/>
-           <img src={marq12} alt="Natural"  class="marq12"/>
-           <img src={komodo}  alt="Natural" />
-           <img src={marq2} alt="Natural"  class="marq2"/>
-           <img src={marq5} alt="Natural"  class="marq5"/>
-           <img src={marq6} alt="Natural"  class="marq6"/>
-           <img src={marq7} alt="Natural"  class="marq7"/>
-           <img src={marq9} alt="Natural"  class="marq9"/>
-           <img src={marq11} alt="Natural"  class="marq11"/>
+       <div className= "marquee-content" aria-hidden="true" >
+            <div>
+                <Image src='/images/boat.jpg' alt="Natural"   height="160px" width="160px"className="boat"/>
+            </div>
+            <div>
+                <Image src='/images/marq1.jpg' alt="Natural"  height="160px" width="160px"  className="marq1"/>
+            </div>
+           <div>
+                <Image src='/images/marq4.jpg' alt="Natural"  height="160px" width="160px"  className="marq4"/>
+           </div>
+           <div>
+                <Image src='/images/beach.jpg'  alt="Natural" height="160px" width="160px"  className="marqBeach"/>
+           </div>
+           <div>
+                <Image src='/images/marq3.jpg' alt="Natural"  height="160px" width="160px"  className="marq3"/>
+           </div>
+           <div>
+                <Image src='/images/marq8.jpg' alt="Natural"  height="160px" width="160px"  className="marq8"/>
+           </div>
+           <div>
+                <Image src='/images/marq10.jpg' alt="Natural" height="160px" width="160px"  className="marq10"/>
+           </div>
+           <div>
+                <Image src='/images/marq12.jpg' alt="Natural" height="160px" width="160px"  className="marq12"/>
+           </div>
+           <div>
+                <Image src='/images/komodo.jpg'  alt="Natural"  height="160px" width="160px"  className="komodo"/>
+           </div>
+           <div>
+                <Image src='/images/marq2.jpg' alt="Natural"    height="160px" width="160px"  className="marq2"/>
+           </div>
+           <div>
+                <Image src= '/images/marq5.jpg' alt="Natural"   height="160px" width="160px"   className="marq5"/>
+           </div>
+           <div>
+                <Image src= '/images/marq6.jpg' alt="Natural"   height="160px" width="160px"   className="marq6"/>
+           </div>
+           <div>
+                <Image src= '/images/marq7.jpg' alt="Natural"   height="160px" width="160px"   className="marq7"/>
+           </div>
+           <div>
+                <Image src= '/images/marq9.jpg' alt="Natural"   height="160px" width="160px"   className="marq9"/>
+           </div>
+           <div>
+                <Image src= '/images/marq11.jpg' alt="Natural" height="160px" width="160px"   classNames="marq11"/>
+           </div>
        </div>
    </div>    
     )
